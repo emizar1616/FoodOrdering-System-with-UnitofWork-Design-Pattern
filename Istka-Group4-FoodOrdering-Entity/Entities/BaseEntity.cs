f7aca6809a9d953b873aa27a4f113329dfbf5a7b
Mustafa KURBAN <mustafa.kurban@isik.edu.tr>
2024-07-01 12:29:07 +0300

    enter base entity

---
+++ Istka-Group4-FoodOrdering-Entity/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Istka_Group4_FoodOrdering_Entity.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
