0a8da844b630d711d227fd8e25c2463883f2e180
Mustafa KURBAN <mustafa.kurban@isik.edu.tr>
2024-07-01 12:02:31 +0300

    Add project files.

---
+++ Istka-Group4-FoodOrdering-WebMvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Istka_Group4_FoodOrdering_WebMvcUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
